81b9356e29aa019231ac10b8484a8e4ddb134417
pandeykriti <kritikapandey9416@gmail.com>
2020-11-27 12:52:59 +0530

    first

---
+++ middleware.js
@@ -0,0 +1,9 @@
+module.exports.isLoggedIn = (req, res, next) => {
+    console.log("Req User..",req.user);
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
+        req.flash('error', 'You must be signed in first!');
+        return res.redirect('/login');
+    }
+    next();
+}
